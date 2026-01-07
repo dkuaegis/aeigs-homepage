3d431f3c712b8197e83c7e82c3cfea87afd05166
Kwon DaeGeun <dk31415@naver.com>
2026-01-07 14:10:47 +0900

    Initial commit from Create React App

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-neutral-600 text-white text-2xl">
+      Tailwind v4 정상 작동
+    </div>
+  )
+}
+
+export default App
